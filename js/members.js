4b4bb966901ddecbfb6b185b364ae23e3c3a2e43
godtfred1000 <125463008+godtfred1000@users.noreply.github.com>
2026-07-11 19:01:40 +0200

    Add files via upload

---
+++ js/members.js
@@ -0,0 +1,289 @@
+window.MEMBERS=[
+  {
+    "name": "Aker Byggentreprenør AS",
+    "address": "Ensjøvn. 14, 0655 Oslo",
+    "phone": "22 57 83 60",
+    "email": "post@norentreprenor.no",
+    "web": ""
+  },
+  {
+    "name": "Akershus Byggentreprenør AS",
+    "address": "Nerdrumveien 57, 1900 Fetsund",
+    "phone": "63 88 51 13",
+    "email": "jenseriksolberg@gmail.com",
+    "web": ""
+  },
+  {
+    "name": "Artea Malermester AS",
+    "address": "Strømsveien 266, 0668 Oslo",
+    "phone": "970 85 471",
+    "email": "post@artea.no",
+    "web": "https://www.artea.no"
+  },
+  {
+    "name": "Bjørn Simonsen & Sønn AS",
+    "address": "Schleppegrellsgt. 7, 0556 Oslo",
+    "phone": "23 12 23 90",
+    "email": "malermester@bjorn-simonsen-sonn.no",
+    "web": "https://www.bjorn-simonsen-sonn.no"
+  },
+  {
+    "name": "Bygg- og Malermestertjenesten AS",
+    "address": "Lienga 6–9, 1414 Trollåsen",
+    "phone": "40 00 10 14",
+    "email": "post@byggogmal.no",
+    "web": "https://www.byggogmal.no"
+  },
+  {
+    "name": "Drøbak Malermesterservice AS",
+    "address": "Holterveien 4 B, 1448 Drøbak",
+    "phone": "64 93 40 00",
+    "email": "post@dmms.as",
+    "web": "https://www.dmms.as"
+  },
+  {
+    "name": "Einar Bjørnstad & Sønn AS",
+    "address": "Kløfterhagen 57, 1067 Oslo",
+    "phone": "22 30 35 38",
+    "email": "ebsoenas@online.no",
+    "web": ""
+  },
+  {
+    "name": "Engen Byggservice AS",
+    "address": "Strømbakken 9, 3046 Drammen",
+    "phone": "32 83 09 97",
+    "email": "stengen@online.no",
+    "web": ""
+  },
+  {
+    "name": "Hunvik & Co AS",
+    "address": "Biskop Jens Nilssøns gate 5, 0659 Oslo",
+    "phone": "900 43 430",
+    "email": "post@hunvik.no",
+    "web": ""
+  },
+  {
+    "name": "Håndverksteknikk AS",
+    "address": "Alnabruveien 9, 0668 Oslo",
+    "phone": "22 88 13 00",
+    "email": "thom@handverksteknikk.no",
+    "web": "https://www.handverksteknikk.no"
+  },
+  {
+    "name": "Kaasa AS",
+    "address": "Tvetenveien 162, 0671 Oslo",
+    "phone": "23 33 24 30",
+    "email": "terje@kaasa.no",
+    "web": ""
+  },
+  {
+    "name": "Kirkestuen AS",
+    "address": "Nils Hansens vei 2C, 0666 Oslo",
+    "phone": "908 71 480",
+    "email": "ole@kirkestuenas.no",
+    "web": ""
+  },
+  {
+    "name": "Lars E. Strand Malermester og Entreprenørforretning AS",
+    "address": "Hvamsvingen 11B, 2013 Skjetten",
+    "phone": "901 26 631",
+    "email": "lars@larses.no",
+    "web": ""
+  },
+  {
+    "name": "Malercompagniet Oslo AS",
+    "address": "Kjelsåsveien 132, 0411 Oslo",
+    "phone": "23 39 18 00",
+    "email": "tom@malercompagniet.no",
+    "web": "https://www.malercompagniet.no"
+  },
+  {
+    "name": "Malerentreprenøren AS",
+    "address": "Smalvollveien 26–28, 0667 Oslo",
+    "phone": "915 45 875",
+    "email": "helge@malerentreprenoren.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Anders Eng AS",
+    "address": "Vallerveien 128, 1346 Gjettum",
+    "phone": "90 06 79 82",
+    "email": "anders@anderseng.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Buer AS",
+    "address": "Hvamsvingen 11, 2013 Skjetten",
+    "phone": "919 20 471",
+    "email": "post@mbhvam.no",
+    "web": "https://www.malermesterbuer.no"
+  },
+  {
+    "name": "Malermester Eckholm AS",
+    "address": "Gamle Rovenvei 150, 1900 Fetsund",
+    "phone": "63 88 02 51",
+    "email": "postmaster@eckholm.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Finn E. Larsen",
+    "address": "Kløversvingen 68, 1405 Langhus",
+    "phone": "90 56 01 56",
+    "email": "felarsen@online.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Gabrielsen AS",
+    "address": "Lundliveien 11, 0584 Oslo",
+    "phone": "905 93 115",
+    "email": "lars@malermesteras.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Jens Petter Lunde AS",
+    "address": "Stanseveien 25, 0976 Oslo",
+    "phone": "22 22 74 22",
+    "email": "bo.lunde@jpl.no",
+    "web": "https://www.jpl.no"
+  },
+  {
+    "name": "Malermester Karl Tore Mikalsen AS",
+    "address": "Beddingen 20, 0250 Oslo",
+    "phone": "906 68 655",
+    "email": "post@malermestermikalsen.no",
+    "web": "https://www.malermestermikalsen.no"
+  },
+  {
+    "name": "Malermester K. Langmo AS",
+    "address": "Ryensvingen 11, 0680 Oslo",
+    "phone": "908 34 560",
+    "email": "kelangmo@online.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Mathisen AS",
+    "address": "Industriveien 8 E, 1473 Lørenskog",
+    "phone": "67 98 98 60",
+    "email": "per@mmmathisen.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester O.E. Samuelsen AS",
+    "address": "Fredensborgveien 66, 0177 Oslo",
+    "phone": "908 84 045",
+    "email": "lasse.samuelsen@gmail.com",
+    "web": ""
+  },
+  {
+    "name": "Malermester Paul-Arne Braathen",
+    "address": "Brennastubben 24, 1279 Oslo",
+    "phone": "906 15 190",
+    "email": "parbra@online.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Per Henning Hovind",
+    "address": "Kyhns vei 2D, 1166 Oslo",
+    "phone": "952 09 495",
+    "email": "pe-h-h@online.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Petter Skovholt AS",
+    "address": "Brobekkveien 115, 0582 Oslo",
+    "phone": "950 88 026",
+    "email": "petter.skovholt@mmps.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Ragnar Torp AS",
+    "address": "Tuterudkroken 20, 2007 Kjeller",
+    "phone": "920 24 255",
+    "email": "ole.torp@ragnartorp.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Rune Malerstuen AS",
+    "address": "Prost Holms vei 165, 2019 Skedsmokorset",
+    "phone": "911 61 705",
+    "email": "rune@malerstuen.no",
+    "web": ""
+  },
+  {
+    "name": "Malermester Thorendahl AS",
+    "address": "Nils Hansens vei 2C, 0666 Oslo",
+    "phone": "23 25 34 00",
+    "email": "firmapost@thorendahl.no",
+    "web": "https://www.thorendahl.no"
+  },
+  {
+    "name": "Malermester Tom Beving AS",
+    "address": "Stanseveien 25, 0976 Oslo",
+    "phone": "23 30 00 20",
+    "email": "post@beving.com",
+    "web": "https://www.beving.com"
+  },
+  {
+    "name": "Malermester Trond H. Eriksen",
+    "address": "Trollhaugen 16 B, 1384 Asker",
+    "phone": "92 06 26 90",
+    "email": "trond.h.eriksen@online.no",
+    "web": ""
+  },
+  {
+    "name": "Malermesterfirmaet Klemet Karlsen AS",
+    "address": "Frydenbergveien 41 A, 0575 Oslo",
+    "phone": "22 20 84 88",
+    "email": "post@klemet-karlsen.no",
+    "web": ""
+  },
+  {
+    "name": "Malermesterfirmaet Pedersen & Sikkerbøl AS",
+    "address": "Smedbergveien 5, 1187 Oslo",
+    "phone": "23 13 47 00",
+    "email": "jan@malerne.no",
+    "web": ""
+  },
+  {
+    "name": "Malermestrene Bjørn S. Nielsen og Sønner ANS",
+    "address": "Lambertveien 17 B, 1160 Oslo",
+    "phone": "93 04 30 76",
+    "email": "post@malernielsen.no",
+    "web": ""
+  },
+  {
+    "name": "Malermestrene Juuhl & Bratfoss AS",
+    "address": "Hovfaret 8, 0275 Oslo",
+    "phone": "22 51 90 00",
+    "email": "post@jubr.no",
+    "web": ""
+  },
+  {
+    "name": "Meyer-Mørch AS",
+    "address": "Ringeriksveien 162B, 1339 Vøyenenga",
+    "phone": "67 17 17 30",
+    "email": "epost@meyer-morch.no",
+    "web": "https://www.meyer-morch.no"
+  },
+  {
+    "name": "PLG Overflateteknikk AS",
+    "address": "Siggerudveien 1047, 1404 Siggerud",
+    "phone": "911 00 476",
+    "email": "firmapost@plg.no",
+    "web": ""
+  },
+  {
+    "name": "Regnbuen Malermesterbedrift AS",
+    "address": "Brobekkveien 115 A, 0582 Oslo",
+    "phone": "23 40 03 70",
+    "email": "post@regnbuen.no",
+    "web": "https://www.regnbuen.no"
+  },
+  {
+    "name": "Servicealliansen Øst AS",
+    "address": "Østre Aker vei 205, 0975 Oslo",
+    "phone": "22 71 50 60",
+    "email": "kontor@besttotal.no",
+    "web": ""
+  }
+];
