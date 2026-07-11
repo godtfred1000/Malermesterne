0d62294dabf4e410f0c449bf1f0f635cb275ad9f
godtfred1000 <125463008+godtfred1000@users.noreply.github.com>
2026-07-11 13:25:09 +0200

    Add files via upload

--- js/medlemmer.js
+++ js/medlemmer.js
@@ -1,1 +1,378 @@
+/*
+  MEDLEMSBEDRIFTER HENTET FRA malermestreneioslo.no/medlemsbedrifter/
+  Oppdatert 11. juli 2026.
 
+  For å legge til en bedrift: kopier en hel blokk mellom { og },
+  lim den inn før den siste ], og husk komma mellom blokkene.
+*/
+window.MEDLEMSBEDRIFTER = [
+  {
+    "navn": "Aker Byggentreprenør AS",
+    "sted": "Oslo",
+    "adresse": "Ensjøvn. 14, 0655 Oslo",
+    "telefon": "22 57 83 60",
+    "epost": "post@norentreprenor.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Akershus Byggentreprenør AS",
+    "sted": "Fetsund",
+    "adresse": "Nerdrumveien 57, 1900 FETSUND",
+    "telefon": "63 88 51 13",
+    "epost": "jenseriksolberg@gmail.com",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Artea Malermester AS",
+    "sted": "Oslo",
+    "adresse": "Strømsveien 266, 0668 Oslo",
+    "telefon": "970 85 471",
+    "epost": "post@artea.no",
+    "nettside": "www.artea.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Bjørn Simonsen & Sønn AS",
+    "sted": "Oslo",
+    "adresse": "Schleppegrellsgt. 7, 0556 OSLO",
+    "telefon": "23 12 23 90",
+    "epost": "malermester@bjorn-simonsen-sonn.no",
+    "nettside": "www.bjorn-simonsen-sonn.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Bygg- og Malermestertjenesten AS",
+    "sted": "Trollåsen",
+    "adresse": "Lienga 6-9, 1414 Trollåsen",
+    "telefon": "40 00 10 14",
+    "epost": "post@byggogmal.no",
+    "nettside": "www.byggogmal.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Drøbak Malermesterservice AS",
+    "sted": "Drøbak",
+    "adresse": "Holterveien 4 B, 1448 DRØBAK",
+    "telefon": "64 93 40 00",
+    "epost": "post@dmms.as",
+    "nettside": "www.dmms.as",
+    "fagomrader": []
+  },
+  {
+    "navn": "Einar Bjørnstad & Sønn AS",
+    "sted": "Oslo",
+    "adresse": "Kløfterhagen 57, 1067 Oslo",
+    "telefon": "22 30 35 38",
+    "epost": "ebsoenas@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Engen Byggservice AS",
+    "sted": "Drammen",
+    "adresse": "Strømbakken 9, 3046 Drammen",
+    "telefon": "32 83 09 97",
+    "epost": "stengen@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Hunvik & Co AS",
+    "sted": "Oslo",
+    "adresse": "Biskop Jens Nilssøns gate 5, 0659 Oslo",
+    "telefon": "900 43 430",
+    "epost": "post@hunvik.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Håndverksteknikk AS",
+    "sted": "Oslo",
+    "adresse": "Alnabruveien 9, 0668 Oslo",
+    "telefon": "22 88 13 00",
+    "epost": "thom@handverksteknikk.no",
+    "nettside": "www.handverksteknikk.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Kaasa AS",
+    "sted": "Oslo",
+    "adresse": "Tvetenveien 162, 0671 Oslo",
+    "telefon": "23 33 24 30",
+    "epost": "terje@kaasa.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Kirkestuen AS",
+    "sted": "Oslo",
+    "adresse": "Nils Hansens vei 2C, 0666 Oslo",
+    "telefon": "908 71 480",
+    "epost": "ole@kirkestuenas.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Lars E. Strand Malermester og Entreprenørforretning AS",
+    "sted": "Skjetten",
+    "adresse": "Hvamsvingen 11B, 2013 Skjetten",
+    "telefon": "901 26 631",
+    "epost": "lars@larses.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malercompagniet Oslo AS",
+    "sted": "Oslo",
+    "adresse": "Kjelsåsveien 132, 0411 OSLO",
+    "telefon": "23 39 18 00",
+    "epost": "tom@malercompagniet.no",
+    "nettside": "www.malercompagniet.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malerentreprenøren A/S",
+    "sted": "Oslo",
+    "adresse": "Smalvollvn. 26-28, 0667 OSLO",
+    "telefon": "915 45 875",
+    "epost": "helge@malerentreprenoren.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Anders Eng AS",
+    "sted": "Gjettum",
+    "adresse": "Vallerveien 128, 1346 Gjettum",
+    "telefon": "90 06 79 82",
+    "epost": "anders@anderseng.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Buer AS",
+    "sted": "Skjetten",
+    "adresse": "Hvamsvingen 11, 2013 Skjetten",
+    "telefon": "919 20 471",
+    "epost": "post@mbhvam.no",
+    "nettside": "www.malermesterbuer.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Eckholm AS",
+    "sted": "Fetsund",
+    "adresse": "Gamle Rovenvei 150, 1900 FETSUND",
+    "telefon": "63 88 02 51",
+    "epost": "postmaster@eckholm.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Finn E. Larsen",
+    "sted": "Langhus",
+    "adresse": "Kløversvingen 68, 1405 Langhus",
+    "telefon": "90 56 01 56",
+    "epost": "felarsen@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Gabrielsen AS",
+    "sted": "Oslo",
+    "adresse": "Lundliveien 11, 0584 OSLO",
+    "telefon": "905 93 115",
+    "epost": "lars@malermesteras.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Jens Petter Lunde AS",
+    "sted": "Oslo",
+    "adresse": "Stanseveien 25, 0976 OSLO",
+    "telefon": "22 22 74 22",
+    "epost": "bo.lunde@jpl.no",
+    "nettside": "www.jpl.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Karl Tore Mikalsen AS",
+    "sted": "Oslo",
+    "adresse": "Beddingen 20, 0250 Oslo",
+    "telefon": "906 68 655",
+    "epost": "post@malermestermikalsen.no",
+    "nettside": "www.malermestermikalsen.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester K. Langmo AS",
+    "sted": "Oslo",
+    "adresse": "Ryensvingen 11, 0680 Oslo",
+    "telefon": "908 34 560",
+    "epost": "kelangmo@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Mathisen AS",
+    "sted": "Lørenskog",
+    "adresse": "Industriveien 8 E, 1473 Lørenskog",
+    "telefon": "679 89 860",
+    "epost": "per@mmmathisen.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester O.E. Samuelsen AS",
+    "sted": "Oslo",
+    "adresse": "Fredensborgveien 66, 0177 OSLO",
+    "telefon": "908 84 045",
+    "epost": "lasse.samuelsen@gmail.com",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Paul-Arne Braathen",
+    "sted": "Oslo",
+    "adresse": "Brennastubben 24, 1279 OSLO",
+    "telefon": "906 15 190",
+    "epost": "parbra@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Per Henning Hovind",
+    "sted": "Oslo",
+    "adresse": "Kyhns vei 2D, 1166 OSLO",
+    "telefon": "952 09 495",
+    "epost": "pe-h-h@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Petter Skovholt AS",
+    "sted": "Oslo",
+    "adresse": "Brobekkveien 115, 0179 Oslo",
+    "telefon": "950 88 026",
+    "epost": "Petter.skovholt@mmps.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Ragnar Torp AS",
+    "sted": "Kjeller",
+    "adresse": "Tuterudkroken 20, 2007 KJELLER",
+    "telefon": "920 24 255",
+    "epost": "ole.torp@ragnartorp.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Rune Malerstuen AS",
+    "sted": "Skedsmokorset",
+    "adresse": "Prost Holms vei 165, 2019 Skedsmokorset",
+    "telefon": "911 61 705",
+    "epost": "rune@malerstuen.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Thorendahl AS",
+    "sted": "Oslo",
+    "adresse": "Nils Hansens vei 2C, 0666 Oslo",
+    "telefon": "23 25 34 00",
+    "epost": "firmapost@thorendahl.no",
+    "nettside": "www.thorendahl.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Tom Beving AS",
+    "sted": "Oslo",
+    "adresse": "Stanseveien 25, 0976 Oslo",
+    "telefon": "23 30 00 20",
+    "epost": "post@beving.com",
+    "nettside": "www.beving.com",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermester Trond H. Eriksen",
+    "sted": "Asker",
+    "adresse": "Trollhaugen 16 B, 1384 ASKER",
+    "telefon": "92 06 26 90",
+    "epost": "trond.h.eriksen@online.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermesterfirmaet Klemet Karlsen AS",
+    "sted": "Oslo",
+    "adresse": "Frydenbergveien 41 A, 0575 Oslo",
+    "telefon": "22 20 84 88",
+    "epost": "post@klemet-karlsen.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermesterfirmaet Pedersen & Sikkerbøl AS",
+    "sted": "Oslo",
+    "adresse": "Smedbergveien 5, 1187 Oslo",
+    "telefon": "23 13 47 00",
+    "epost": "jan@malerne.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermestrene Bjørn S. Nielsen og Sønner ANS",
+    "sted": "Oslo",
+    "adresse": "Lambertveien 17 B, 1160 Oslo",
+    "telefon": "93 04 30 76",
+    "epost": "post@malernielsen.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Malermestrene Juuhl & Bratfoss AS",
+    "sted": "Oslo",
+    "adresse": "Hovfaret 8, 0275 OSLO",
+    "telefon": "22 51 90 00",
+    "epost": "post@jubr.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Meyer-Mørch AS",
+    "sted": "Vøyenenga",
+    "adresse": "Ringeriksveien 162B, 1339 VØYENENGA",
+    "telefon": "67 17 17 30",
+    "epost": "epost@meyer-morch.no",
+    "nettside": "www.meyer-morch.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "PLG Overflate-teknikk AS",
+    "sted": "Siggerud",
+    "adresse": "Siggerudveien 1047, 1404 Siggerud",
+    "telefon": "911 00 476",
+    "epost": "firmapost@plg.no",
+    "nettside": "",
+    "fagomrader": []
+  },
+  {
+    "navn": "Regnbuen Malermesterbedrift AS",
+    "sted": "Oslo",
+    "adresse": "Brobekkveien 115 A, 0582 OSLO",
+    "telefon": "23 40 03 70",
+    "epost": "post@regnbuen.no",
+    "nettside": "www.regnbuen.no",
+    "fagomrader": []
+  },
+  {
+    "navn": "Servicealliansen Øst AS",
+    "sted": "Oslo",
+    "adresse": "Østre Aker vei 205, 0975 Oslo",
+    "telefon": "22 71 50 60",
+    "epost": "kontor@besttotal.no",
+    "nettside": "",
+    "fagomrader": []
+  }
+];
